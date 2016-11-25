2b3f44574838821e210d0b1fa5adda08ec2d3b19
Deric Pang <dericp@cs.washington.edu>
2016-11-25 15:35:04 +0100

    initial implementation of PEGASOS

--- parallel_stochastic_gradient_descent.py
+++ parallel_stochastic_gradient_descent.py
@@ -1,76 +1,72 @@
 import numpy as np
-from numpy import linalg as LA
+import math
 
-def list2array(list):
-    input_2darray = np.zeros([5000,401])# this [5000,401] could be a more flexible coding
-    for i in range(len(list)):
-        token = list[i].split(' ')
-        for j in range(len(token)):
-            input_2darray[i,j] = float(token[j])
-    return input_2darray
 
+# transforms X into m-dimensional feature vectors using RFF and RBF kernel
 def transform(X):
     # Make sure this function works for both 1D and 2D NumPy arrays.
-    return X
+    # m is the dimension of the transformed feature vector
+    m = 1000
 
-def computeFF(x,y,m): # this code is for RBF kernel
-    # the input the x,y are d-dim; want to map them into D dim; then "inverse" them to m dim
-    # output: the expectation of zx*zy, i.e. the approximate k(x-y)
-    #rand_feat_w = np.zeros([m,D])# initialize the feature matrice
-    #rand_feat_b = np.zeros([m,1])
-    zxzy = 0
-    for i in range(m):
-        w = np.random.normal(0,1,[D,1])
-        b = np.pi*np.random.rand(1)
-        zx = np.sqrt(2)*np.cos(np.inner(w,x)+b)
-        zy = np.sqrt(2)*np.cos(np.inner(w,y)+b)
-        #rand_feat_w[i,:] = np.transpose(w)
-        #rand_feat_b[i,:] = np.transpose(b)
-        zxzy += zx*zy/m
-    return zxzy
+    if X.ndim == 1:
+        w = np.random.randn(m)
+        return math.sqrt(2) * np.cos(np.inner(w, x) + b)
+    else:
+        ret = np.zeros([X.shape[0], m])
+        for i in range(X.shape[0]):
+            w = np.random.randn(m)
+            b = np.random.rand() * 2 * np.pi
+            ret[i, :] = math.sqrt(2) * np.cos(np.inner(w, X[i]) + b)
+        return ret
 
-# def extract_nystrom_feat(X,m):
-#     # input: X is n*d np.array contains n features: x1,...,xn; m is the size of sampling
-#     n = X.shape[0]
-#     rand_idx = np.random.choice(m,m,replace=False)
-#     S = X[rand_idx,:]
-#     KSS = np.zeros([m,m])
-#     D1 = 10*m # hard code
-#     m1 = 2*m # hard code
-#     for i in range(m):
-#         KSS[i,i] = computeFF(S[i,:],S[i,:],m1,D1)
-#         for j in range(i+1,m,1):
-#             KSS[i,j] = computeFF(S[i,:],S[j,:],m1,D)
-#             KSS[j,i] = KSS[i,j]
-#     eigvalue,eigvectors = LA.eig(KSS)
-#     D = np.diag(eigvalue)
-#     Khat = np.zeros([n,n])
-#     for i in range(n):
-#         KSi = np,zeros([m,1])
-#         for k in range(m):
-#             KSi[j] = computeFF(X[i,:],S[k,:],m1,D1)
-#         zxi = np.matmul(1./np.sqrt(D),KSi)
-#         Khat[i,i] = np.inner(zxi,zxi)
-#         for j in range(n):
-#             # maybe there's a faster way than compute the k(xi,sj) again ina for loop
-#             for k in range(m):
-#                 KSj = computeFF(X[i,:],S[k,:],m1,D1)
-#             zxj = np.matmul(1./np.sqrt(D),KSj)
-#             Khat[i,j] = np.inner(zxi,zxj)
-#             Khat[j,i] = np.inner(zxi,zxj)
-#     return Khat
 
 def mapper(key, value):
     # key: None
     # value: one line of input file
-    value = list2array(value)
-    for line in value:
-     #   tokens = line.split()
-        y = line[0]
-        features = line[1:]
 
-        print len(features)
+    # 2D NumPy array containing the original feature vectors
+    features = np.zeros([5000,400])# this [5000,401] could be a more flexible coding
+    # 1D NumPy array containing the classifications of the training data
+    classifications = np.zeros(5000)
+
+    # populate features and classifications
+    for i in range(len(list)):
+        tokens = list[i].split()
+        classifications[i] = tokens[0]
+        for j in range(1, len(tokens)):
+            features[i, j] = float(tokens[j])
+
+    # project features into higher dimensional space
+    features = transform(features)
+
+    iterations = 50
+    w = np.zeros(1000)
+    t = 1
+    for i in range(iterations):
+        w = update_weights(w, features, classifications, t)
+        t += 1
+
     yield "key", "value"  # This is how you yield a key, value pair
+
+
+def update_weights(w, features, classifications, t):
+    LAMBDA = 0.001
+    eta = 1 / (t * LAMBDA)
+
+    # let's find delta t
+    summed_loss = 0
+    for i in range(features.shape[0]):
+        summed_loss += hinge_loss(w, features[i], classifications[i])
+    delta_t = LAMBDA * w - (eta / 5000) * summed_loss
+
+    w_prime = w - eta * delta_t
+
+    return min(1, (1 / math.sqrt(LAMBDA) / w_prime.size)) * w_prime
+
+
+def hinge_loss(w, x, y):
+    max(0, 1 - y * np.inner(w, x))
+
 
 
 def reducer(key, values):
