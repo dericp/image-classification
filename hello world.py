9a7c25d815b68bf0986908cf3137e610195c4a86
Deric Pang <dericp@cs.washington.edu>
2016-11-29 15:00:52 +0100

    Removed toy file.

--- hello world.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-def list2array(list):
-    input_2darray = np.zeros([5000,401])
-    for i in range(len(list)):
-        token = list[i].split(' ')
-        for j in range(len(token)):
-            input_2darray[i,j] = float(token[j])
-    return input_2darray
